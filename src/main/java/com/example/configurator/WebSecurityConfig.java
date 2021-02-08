df24429327d12f1f8705d5df7147cdfb93d16d31
OCiqPXeh <mike.galitsky@gmail.com>
2021-02-08 08:40:02 +0100

    Refactoring

--- src/main/java/com/example/configurator/WebSecurityConfig.java
+++ src/main/java/com/example/configurator/WebSecurityConfig.java
@@ -11,6 +11,7 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 
@@ -49,7 +50,8 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.userDetailsService(userDetailsService)
-                .passwordEncoder(passwordEncoder);  // Easy for development release : .passwordEncoder(NoOpPasswordEncoder.getInstance());
+                // .passwordEncoder(passwordEncoder);   Easy for development release :
+                .passwordEncoder(NoOpPasswordEncoder.getInstance());
 
     }
 }
